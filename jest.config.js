7bf1a6e4dc7f597d75a82db16541f1fb1d303ee3
Jeffrey Zhen <jzhen91@gmail.com>
2019-04-03 23:16:53 -0400

    add circle and jest to lint-staged

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
   ],
   snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
