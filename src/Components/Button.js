e407636db3d464b234104ff4aca16b2482037218
Jeffrey Zhen <jzhen91@gmail.com>
2019-04-03 21:18:31 -0400

    add more Button stories

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
-export const Button = () => <button type="submit">Submit</button>;
+const StyledButton = styled.button`
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  cursor: pointer;
+
+  :disabled {
+    cursor: default;
+  }
+`;
+
+export const Button = ({ children, ...rest }) => (
+  <StyledButton {...rest}>{children}</StyledButton>
+);
+
+Button.defaultProps = {
+  children: '',
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Button;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,23 +4,16 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-import MyButton from '../src/Components/Button';
+import { Welcome } from '@storybook/react/demo';
+import { Button } from '../src/Components/Button';
+
+const storiesOfButton = storiesOf('Button', module);
 
 storiesOf('Welcome', module).add('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
 ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
-storiesOf('MyButton', module).add('Default', () => <MyButton />);
+storiesOfButton
+  .add('with text', () => <Button onClick={action('clicked')}>Click me</Button>)
+  .add('loading', () => <Button disabled>Loading...</Button>)
+  .add('disabled', () => <Button disabled>Click me</Button>);
