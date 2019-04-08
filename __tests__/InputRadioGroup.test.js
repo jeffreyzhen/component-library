2e090ad5ec28a00549854544dbfe5906ffd885d3
Jeffrey Zhen <jzhen91@gmail.com>
2019-04-08 14:45:33 -0400

    test: fix inputradiogroup tests

--- __tests__/InputRadioGroup.test.js
+++ __tests__/InputRadioGroup.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
-import { InputRadio } from '../src/Components/InputRadio';
 import { InputRadioGroup } from '../src/Components/InputRadioGroup';
 
 const yesInputRadio = {
   id: 'input-yes',
   name: 'yes-no',
   value: 'yes',
+  label: 'Yes',
 };
 
 const noInputRadio = {
   id: 'input-no',
   name: 'yes-no',
   value: 'no',
+  label: 'No',
 };
 
 const radioGroupTestId = 'yes-no-radio';
@@ -20,8 +21,7 @@ const radioGroupTestId = 'yes-no-radio';
 it('should render two InputRadios', () => {
   const { getByLabelText, getByTestId } = render(
     <InputRadioGroup data-testid={radioGroupTestId}>
-      <InputRadio {...yesInputRadio}>Yes</InputRadio>
-      <InputRadio {...noInputRadio}>No</InputRadio>
+      {[yesInputRadio, noInputRadio]}
     </InputRadioGroup>
   );
 
@@ -37,10 +37,7 @@ it('should render two InputRadios', () => {
 
 it('should only have one checked out of the group', () => {
   const { getByLabelText } = render(
-    <InputRadioGroup>
-      <InputRadio {...yesInputRadio}>Yes</InputRadio>
-      <InputRadio {...noInputRadio}>No</InputRadio>
-    </InputRadioGroup>
+    <InputRadioGroup>{[yesInputRadio, noInputRadio]}</InputRadioGroup>
   );
 
   const yesRadio = getByLabelText('Yes');
